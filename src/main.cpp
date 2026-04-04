46c547fde41561fd4cf66d23e24d3f9dce1ce8f5
Adam Dziwiszek <adziwiszek1@gmail.com>
2026-04-04 22:11:24 +0200

    first working grid

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,13 @@
 #include <glad.h>
 #include <GLFW/glfw3.h>
-#include <glm/vec3.hpp>
-#include <glm/glm.hpp>
 #include <Shader.hpp>
-#include <iostream>
-#include <vector>
 #include <array>
-
-std::vector<glm::vec3> vertices;
-std::vector<unsigned int> indices;
-
-unsigned int vertexBuffer;
-unsigned int vertexArray;
-
-unsigned int elementBuffer;
-
-/* source: https://faun.pub/draw-circle-in-opengl-c-2da8d9c2c103 
- * access: 03.04.2026 */
-void buildCircle(float radius, int vCount) {
-  float angle = 360.0f / vCount;
-
-  int triangleCount = vCount - 2;
-
-  // positions
-  for (int i = 0; i < vCount; i++)
-  {
-      float currentAngle = angle * i;
-      float x = radius * cos(glm::radians(currentAngle));
-      float y = radius * sin(glm::radians(currentAngle));
-      float z = 0.0f;
-
-      vertices.push_back(glm::vec3(x, y, z));
-  }
-
-  for (int i = 0; i < triangleCount; i++)
-  {
-      indices.push_back(0);
-      indices.push_back(i + 1);
-      indices.push_back(i + 2);
-  }
-}
+#include <glm/glm.hpp>
+#include <glm/vec3.hpp>
+#include <iostream>
+#include <print>
+#include <chrono>
+#include <vector>
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
   glViewport(0, 0, width, height);
@@ -50,7 +18,7 @@ void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
 constexpr int NDIM = 2;
 
 /* Origin */
-constexpr std::array<float, NDIM>        O = {0.0f, 0.0f};
+constexpr std::array<float, NDIM> O = {0.0f, 0.0f};
 /* Size of the grid */
 constexpr std::array<unsigned int, NDIM> L = {10, 10};
 /* Number of cells in each coordinate */
@@ -59,15 +27,12 @@ constexpr std::array<unsigned int, NDIM> N = {10, 10};
 /* Size of each voxel */
 constexpr auto D = [] {
   std::array<float, NDIM> d{};
-  for(int i = 0; i < NDIM; i++)
+  for (int i = 0; i < NDIM; i++)
     d[i] = static_cast<float>(L[i]) / N[i];
   return d;
 }();
 
-glm::mat4 proj = glm::ortho(
-    O[0], O[0] + L[1],
-    O[1], O[1] + L[1]
-);
+glm::mat4 proj = glm::ortho(O[0], O[0] + L[1], O[1], O[1] + L[1]);
 
 /* Class to manage simulation grid.
  *
@@ -77,7 +42,7 @@ glm::mat4 proj = glm::ortho(
  * solid tells us if given square is a solid. If so we don't take use it in the
  * simulation. */
 class FluidGrid {
-private:
+  std::array<std::array<int, N[0]>, N[1]> smoke{};
   float overRelaxation{1.9};
 public:
   std::array<std::array<float, N[0] + 1>, N[1]> velocityX{};
@@ -85,31 +50,109 @@ public:
   std::array<std::array<int, N[0] + 2>, N[1] + 2> solid{};
 
   void solveIncompressibility(int numIter, float dt) {
-    for(int ni = 0; ni < numIter; ni++) {
-      for(int row = 1; row < N[1]; row++) {
-        for(int col = 1; col < N[0]; col++) {
-          if(solid[row][col])
+    float total_div = 0;
+    for (int ni = 0; ni < numIter; ni++) {
+      for (int row = 1; row < N[1]; row++) {
+        for (int col = 1; col < N[0]; col++) {
+          if (solid[row][col])
             continue;
 
-          auto sl = solid[row][col-1];
-          auto sr = solid[row][col+1];
-          auto su = solid[row-1][col];
-          auto sd = solid[row+1][col];
+          auto sl = solid[row][col - 1];
+          auto sr = solid[row][col + 1];
+          auto su = solid[row - 1][col];
+          auto sd = solid[row + 1][col];
           auto s = sl + sr + su + sd;
-          if(s == 0.0)
+          if (s == 0.0)
             continue;
 
-          float d = velocityX[row][col] - velocityX[row][col-1] +
-                   velocityY[row-1][col] - velocityY[row][col];
-          float p = overRelaxation * (- d / s);
+          float d = velocityX[row][col] - velocityX[row][col - 1] +
+                    velocityY[row - 1][col] - velocityY[row][col];
+          total_div += d;
+          float p = overRelaxation * (-d / s);
 
-          velocityX[row][col]   -= sr * p;
-          velocityX[row][col-1] -= sl * p;
-          velocityY[row-1][col] -= su * p;
-          velocityY[row][col]   -= sd * p;
+          velocityX[row][col] -= sr * p;
+          velocityX[row][col - 1] -= sl * p;
+          velocityY[row - 1][col] -= su * p;
+          velocityY[row][col] -= sd * p;
         }
       }
     }
+    std::cout << "divergence = " << total_div << "\r";
+    
+  }
+
+  void advectVelocity(float dt) {
+  }
+
+  void advectSmoke(float dt) {
+  }
+
+  void placeSmoke(float x, float y, float radius) {
+  }
+};
+
+class GridRenderer {
+private:
+  const FluidGrid& grid;
+  std::vector<glm::vec3> vertices;
+  std::vector<unsigned int> indices;
+
+  unsigned int vertexBuffer;
+  unsigned int vertexArray;
+
+  unsigned int elementBuffer;
+public:
+  GridRenderer(const FluidGrid& grid) : grid{grid} {
+    glGenVertexArrays(1, &vertexArray);
+    glGenBuffers(1, &vertexBuffer);
+
+    // element buffer object
+    glGenBuffers(1, &elementBuffer);
+
+    glBindVertexArray(vertexArray);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+
+  }
+   
+  void buildGrid() {
+    indices.clear();
+    vertices.clear();
+
+    for(unsigned int i = 0; i <= N[0]; i++) {
+      for(unsigned int j = 0; j <= N[1]; j++) {
+        float x = O[0] + i * D[0];
+        float y = O[1] + j * D[1];
+        vertices.push_back({x, y, 0});
+      }
+    }
+    for(unsigned int i = 0; i < N[0]; i++) {
+      for(unsigned int j = 0; j < N[1]; j++) {
+        indices.push_back(i + j * (N[0]+1));
+        indices.push_back(i + 1 + j * (N[0]+1));
+        indices.push_back(i + 1 + (j + 1) * (N[0]+1));
+
+        indices.push_back(i + j * (N[0]+1));
+        indices.push_back(i + (j + 1) * (N[0]+1));
+        indices.push_back(i + 1 + (j + 1) * (N[0]+1));
+      }
+    }
+
+    // copying
+    glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec3) * vertices.size(), 
+                 &vertices[0], GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementBuffer);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * indices.size(), 
+                 &indices[0], GL_STATIC_DRAW);
+  }
+
+  void draw() {
+    glBindVertexArray(vertexArray);
+    glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
   }
 };
 
@@ -138,52 +181,37 @@ int main() {
     return -1;
   }
 
-  Shader shader("shaders/vertexShader.glsl", "shaders/fragmentShader.glsl");
-
-  buildCircle(0.1, 32);
-  glm::vec3 circleCenter = {5, 5, 0};
-
-  // buffer
-  glGenVertexArrays(1, &vertexArray);
-  glGenBuffers(1, &vertexBuffer);
-
-  // element buffer object
-  glGenBuffers(1, &elementBuffer);
-
-  glBindVertexArray(vertexArray);
-  glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
-
-  // copying
-  glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec3) * vertices.size(),
-      &vertices[0], GL_STATIC_DRAW);
-
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
-
-  glEnableVertexAttribArray(0);
-
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementBuffer);
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * indices.size(),
-      &indices[0], GL_STATIC_DRAW);
-
-
   glViewport(0, 0, 800, 800);
   glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+  Shader shader("shaders/vertexShader.glsl", "shaders/fragmentShader.glsl");
+
+  FluidGrid simulation;
+  GridRenderer renderer(simulation);
+  renderer.buildGrid();
+
   bool simulating = true;
+  auto prevTime = std::chrono::high_resolution_clock::now();
 
   while (!glfwWindowShouldClose(window)) {
+    auto currentTime = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<float> dtChrono = currentTime - prevTime;
+    prevTime = currentTime;
+    float dt = dtChrono.count();
+    std::cout << "delta time = " << dt << "\r";
+
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
-    if(simulating) {
+    if (simulating) {
+      simulation.solveIncompressibility(10, dt);
     }
 
     shader.use();
     shader.setMat4("proj", proj);
-    shader.setVec3("offset", circleCenter);
+    // shader.setVec3("offset", circleCenter);
+    renderer.draw();
 
-    glBindVertexArray(vertexArray);
-    glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
 
     glfwSwapBuffers(window);
     glfwPollEvents();
